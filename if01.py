f705c86abf7e09b728c76a0a76f6baa05fe8fd0a
ghanshyamdambhalya <ghanshyam.dambhaliya@gmail.com>
2023-01-02 19:19:16 +0530

    python b5

---
+++ if01.py
@@ -0,0 +1,8 @@
+#write a program to accept age from user in years. display message "you are eligible for voting" if age is above or equal to 18
+
+
+age = int(input(" Enter your age "))
+if age >= 18 :
+    print("you are eligible for votiing")
+    
+print("good bye");
---
+++ whilelp3.py
@@ -0,0 +1,21 @@
+#2, 1, 3, 4, 7, 11, 18, 29, 47, 76, 123 .... 300
+
+number1=2
+print(number1)
+number2=1
+print(number2)
+number3=number1+number2
+print(number3)
+number4=number3+number2
+
+#while number<300:
+number=(number1-1)+(number2+1)
+   # print(number)
+#print(number)
+#number=number1+number2
+#print(number)
+#number=number
+#print(number)
+#number=number+3
+#print(number)
+#number=number+4
